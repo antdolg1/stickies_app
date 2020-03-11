b019f4810b40d297cc18d3a1129d87eb27cdb2ca
Antons.Dolgopolovs <antdolg@gmail.com>
2020-03-11 16:32:24 +0200

    Project base created

---
+++ server.js
@@ -0,0 +1,14 @@
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const config = require("./webpack.config");
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(8080, "localhost", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Listening at http://localhost:8080/");
+});
